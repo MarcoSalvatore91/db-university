deb5a1b339868215f032b65f0590abf64a3643c6
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-09 15:40:11 +0100

    cfu > 10

--- select.sql
+++ select.sql
@@ -3,3 +3,9 @@
 SELECT `name`, `surname`, `date_of_birth` AS `year`
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+
+SELECT `name`, `cfu` AS `credits`
+FROM `courses`
+WHERE `cfu` > 10
