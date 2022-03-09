093363fa051eaacbcbb593e5e4dbfd0e2987cff1
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-09 17:27:05 +0100

    count teachers in same office

--- group_by.sql
+++ group_by.sql
@@ -3,3 +3,9 @@
 SELECT COUNT(*) AS 'number_enrolment', YEAR(`enrolment_date`) AS `year`
 FROM `students`
 GROUP BY `year`
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) AS 'num_teachers', `office_number` AS `office`
+FROM `teachers`
+GROUP BY `office` 
