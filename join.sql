d54b7da6e0aad946b14cfd886088173defa01ebb
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-10 15:49:45 +0100

    courses of teacher Fulvio Amato

--- join.sql
+++ join.sql
@@ -13,3 +13,13 @@ FROM `departments`
 JOIN `degrees`
 ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `teachers`.`name` AS `teacher_name`, `teachers`.`surname` AS `teacher_surname`, `courses`.`name` AS `courses_name` 
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44
